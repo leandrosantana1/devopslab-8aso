734f086f6ac09899bff293fe2deb107dc6701e47
leandrosantana1 <leandrosantana@movida.com.br>
2022-09-05 23:39:57 -0300

    05-DEVSECOPS

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
+import os
 
 app = Flask(__name__)
 
+csrf = CSRFProtect(app)
+
 @app.route("/")
 def pagina_inicial():
-    return "Esta pipeline taaaaa um sucesso"
+    return "Laboratório DevOps - FIAP 8ASO v02"
 
 if __name__ == '__main__':
-    app.run()
+    port = os.getenv('PORT')
+    app.run('0.0.0.0', port=port)
